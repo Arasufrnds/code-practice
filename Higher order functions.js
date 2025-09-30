4e061bc47f66d6c2633baa6d69608199077cf4b7
arasufrnds <arasufrnds@gmail.com>
2025-09-30 18:24:22 +0530

    changes

--- Higher order functions.js
+++ Higher order functions.js
@@ -43,10 +43,53 @@ let doubledChars = chars1.map(number => number * 2);
 console.log(doubledChars);
 
 
-//find above 18
+//find above 18 
 let age = [2,43,12,23,45]
 age.forEach(person=>{
     if(person > 18){
         console.log(person)
     }
 })
+
+
+
+//handle higher order function in nested objects
+const data = {
+  users: [
+    { name: "Alice", age: 25, hobbies: ["reading", "cycling"] },
+    { name: "Bob", age: 30, hobbies: ["gaming", "hiking"] },
+    { name: "Charlie", age: 22, hobbies: ["swimming"] }
+  ]
+};
+
+//to extract name, age & hobbies using map method
+let value = data.users.map(emp=>emp.name);
+console.log(value)
+
+//using flatMap take all values of hobbies in single array
+let value = data.users.flatMap(emp=>emp.hobbies);
+console.log(value)
+
+//getting the value of the user having hobby as cycling
+
+let value = [];
+data.users.forEach((user) =>{
+    if(user.hobbies.includes("cycling")){
+        value.push(user.name);
+    }
+})
+
+console.log(value);
+
+//if the user not having nested array then use == if its nested so use includes
+let value = [];
+data.users.forEach((arr)=>{
+    if(arr.age == "25"){
+        value.push(arr.name);
+    }
+})
+console.log(value);
+
+//using filter & map instead of foreach
+let value = data.users.filter(emp=>emp.hobbies.includes("cycling")).map(emp=>emp.name);
+console.log(value);
